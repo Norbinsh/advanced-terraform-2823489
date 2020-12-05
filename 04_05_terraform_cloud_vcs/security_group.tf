954292ff44c67d162b21fc5e78c231f9f5a08e15
Norbinsh <wowshay@gmail.com>
2020-12-05 10:58:39 +0200

    fix formatting

--- 04_05_terraform_cloud_vcs/security_group.tf
+++ 04_05_terraform_cloud_vcs/security_group.tf
@@ -6,16 +6,16 @@ resource "aws_security_group" "sg_frontend" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
